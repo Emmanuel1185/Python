8f16cc871a5cba58e3f8e1f70c2357a5557eff4b
Emmanuel Adefemi <emmanuel.adefemi@gmail.com>
2025-01-18 14:18:40 +0000

    everything from python technical lessons

--- main.py
+++ main.py
@@ -254,10 +254,11 @@ counter = 1
 # Comparison Operator != means not equal to == means equal to
 while guess != targetNumber:
     print("Sorry, that's not it.")
-    counter += 1 # Increment the counter
+    counter += 1 # Increment in the counter
     guess = random.randint(0, 750)
     print("Guess again as your guess was ",guess)
     
     
 print("You got it after ",counter," tries!")
+
 
